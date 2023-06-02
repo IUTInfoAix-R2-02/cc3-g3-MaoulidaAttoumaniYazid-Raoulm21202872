3b71faf524e2b07da818fe0f7b29f04c27e44713
MaoulidaRaoul <yazid-raoul.MAOULIDA-ATTOUMANI@etu.univ-amu.fr>
2023-06-02 16:20:40 +0200

    Partie 1

--- src/main/java/fr/amu/iut/cc3/ToileApp.java
+++ src/main/java/fr/amu/iut/cc3/ToileApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,7 +15,7 @@ public class ToileApp extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("toile.fxml"));
         primaryStage.setResizable(false);
-        primaryStage.setScene( new Scene(root) );
+        primaryStage.setScene( new Scene(root));
         primaryStage.show();
     }
 
--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -11,12 +11,14 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.net.URL;
@@ -30,6 +32,41 @@ public class ToileController implements Initializable {
     private static int angleEnDegre = 60;
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
+    @FXML
+    int point1 = 0;
+    @FXML
+    int point2 = 0;
+    @FXML
+    int point3 = 0;
+    @FXML
+    int point4 = 0;
+    @FXML
+    int point5 = 0;
+    @FXML
+    int point6 = 0;
+
+
+    @FXML
+    TextField competence1;
+    @FXML
+    String Competence1 = "Competence 1";
+
+    @FXML
+    TextField competence2;
+    @FXML
+    TextField competence3;
+    @FXML
+    TextField competence4;
+    @FXML
+    TextField competence5;
+    @FXML
+    TextField competence6;
+    @FXML
+    Button Bouton1;
+    @FXML
+    Circle point ;
+
+    @FXML
 
 
     @Override
@@ -45,5 +82,7 @@ public class ToileController implements Initializable {
         return (int) (rayonCercleExterieur - Math.sin(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
                 *  (value / noteMaximale));
     }
+    public void PosPoint(ActionEvent actionEvent) {
 
+    }
 }
